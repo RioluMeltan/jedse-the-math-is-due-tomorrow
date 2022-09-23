08dc506b5dbf9e3ca06460bbba6e46f98509d268
RioluMeltan <thecodingchemist55@gmail.com>
2022-09-23 14:40:06 +0800

    Initial Commit

---
+++ jedse the math is due tomorrow/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  jedse the math is due tomorrow
+//
+//  Created by Trent Chen on 23/9/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ jedse the math is due tomorrowUITests/jedse_the_math_is_due_tomorrowUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  jedse_the_math_is_due_tomorrowUITestsLaunchTests.swift
+//  jedse the math is due tomorrowUITests
+//
+//  Created by Trent Chen on 23/9/2022.
+//
+
+import XCTest
+
+final class jedse_the_math_is_due_tomorrowUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
